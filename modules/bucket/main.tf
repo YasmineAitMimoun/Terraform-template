d2b6b063eb4cec0f12ad1b9d5e910588ef62c4df
Yasmine <yasmine.aitmimoun@gmail.com>
2026-02-20 22:15:45 +0100

    add bucket versionning

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -3,5 +3,8 @@ resource "google_storage_bucket" "CS_bucket" {
   name     = var.bucket_name
   project = var.project_id
   storage_class = var.storage_class
+  versioning {
+    enabled = true
+  }
   uniform_bucket_level_access = true
 }
